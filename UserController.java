e4bc3b77a6323522a6b5de57debd124f28e9c73f
psn2615 <1sunrise@naver.com>
2020-08-19 12:49:44 +0900

    Add UserController [Delete]

--- UserController.java
+++ UserController.java
@@ -24,5 +24,13 @@ public class UserController {
 
         return "user/update";
     }
+
+   // 회원삭제 페이지로 이동
+    @RequestMapping("/user/delete.do")
+    public String delete() {
+        logger.info("경로 : delete");
+
+        return "user/delete";
+    }
      
 }
